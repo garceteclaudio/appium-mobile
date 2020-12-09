dca5444006d82dcdf1b13d44371cc85b56fb623a
Claudio Garcete <cgarcete@technisys.com>
2020-12-09 15:32:04 -0300

    se quito automationName: 'uiautomator2',

--- config/android.config.js
+++ config/android.config.js
@@ -1,14 +1,17 @@
-const {config} = require('./wdio.conf');
-const AndroidInfo = require('./android.info');
-const path = require('path');
+const { config } = require("./wdio.conf");
+const AndroidInfo = require("./android.info");
+const path = require("path");
 
 // Appium capabilities
-config.capabilities = [{
-    platformName: 'Android',
-    // noReset: false,
-    fullReset: true,
+
+//Todos los permisos true, noReset=false y fullRset comentado
+config.capabilities = [
+  {
+    platformName: "Android",
+    noReset: false,
+    // fullReset: true,
     //maxInstances: 1,
-    automationName: 'uiautomator2',
+    // automationName: 'uiautomator2',
     //deviceName: AndroidInfo.deviceName(),
     udid: AndroidInfo.UDID(),
     platformVersion: AndroidInfo.platFormVersion(),
@@ -16,12 +19,12 @@ config.capabilities = [{
     appPackage: "com.loginexample",
     appActivity: "com.loginexample.MainActivity",
     newCommandTimeout: "8000",
-}];
+  },
+];
 
-config.cucumberOpts.tagExpression = '@androidApp'; // pass tag to run tests specific to android
+config.cucumberOpts.tagExpression = "@androidApp"; // pass tag to run tests specific to android
 
 exports.config = config;
 
 //emulator -list-avds
 // emulator -avd Pixel_3_API_29
-
