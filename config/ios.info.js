e31dc62be12dcd44966fb79bac6b16dbbf1361bf
Claudio Garcete <cgarcete@technisys.com>
2020-12-22 17:22:59 -0300

    change device name ios

--- config/ios.info.js
+++ config/ios.info.js
@@ -1,20 +1,18 @@
 class IosInfo {
-
-    // figura con el siguiente script en mac:
-    // xcrun simctl list | egrep '(Booted)'
-    // instruments -s devices
-    static deviceName() {
-      return "iphone11ios13"; // pass the device name
-    }
-  
-    static platFormVersion() {
-      return "13.0"; // pass the platform version
-    }
-  
-    static appName() {
-      return "loginExample.app"; // pass the bundleid or ipa name
-    }
+  // figura con el siguiente script en mac:
+  // xcrun simctl list | egrep '(Booted)'
+  // instruments -s devices
+  static deviceName() {
+    return "iPhone 11 Pro Max (14.3)"; // pass the device name
   }
-  
-  module.exports = IosInfo;
-  
+
+  static platFormVersion() {
+    return "13.0"; // pass the platform version
+  }
+
+  static appName() {
+    return "loginExample.app"; // pass the bundleid or ipa name
+  }
+}
+
+module.exports = IosInfo;
