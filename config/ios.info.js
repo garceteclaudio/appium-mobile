7e4f9c1055cd0bcb1ae902eb771197f8b934506c
Claudio Garcete <cgarcete@technisys.com>
2020-12-22 17:47:37 -0300

    change capabilities

--- config/ios.info.js
+++ config/ios.info.js
@@ -2,10 +2,12 @@ class IosInfo {
   // figura con el siguiente script en mac:
   // xcrun simctl list | egrep '(Booted)'
   // instruments -s devices
-  static deviceName() {
-    return "iPhone 11 Pro (14.3)"; // pass the device name
-  }
 
+  //iPhone 11 Pro (14.3)
+  static deviceName() {
+    return "iPhone 11 Pro"; // pass the device name
+  }
+  // 14.3
   static platFormVersion() {
     return "14.3"; // pass the platform version
   }
