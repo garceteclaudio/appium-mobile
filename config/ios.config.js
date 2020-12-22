ca38d2d6b6e5fa73c5f6e371fab91e96af20ce6e
Claudio Garcete <cgarcete@technisys.com>
2020-12-22 16:13:40 -0300

    added headless ios config

--- config/ios.config.js
+++ config/ios.config.js
@@ -1,24 +1,26 @@
-const {config} = require('./wdio.conf');
-const IosInfo = require('./ios.info');
-const path = require('path');
+const { config } = require("./wdio.conf");
+const IosInfo = require("./ios.info");
+const path = require("path");
 
 // Appium capabilities
-config.capabilities = [{
-    bundleId: "org.reactjs.native.example.loginExample",
-    platformName: 'iOS',
-    noReset: false,
-    // fullReset: false,
-    automationName: 'XCUITest',
-    udid: "65D5A6EE-FBF1-4C15-A277-6F3C88D9A335",
+config.capabilities = [
+  {
+    appWaitDuration: 50000,
+    // bundleId: "org.reactjs.native.example.loginExample",
+    platformName: "iOS",
+    noReset: true,
+    // fullReset: true,
+    automationName: "XCUITest",
+    // udid: "65D5A6EE-FBF1-4C15-A277-6F3C88D9A335",
     deviceName: IosInfo.deviceName(),
     app: path.resolve(`./apps/${IosInfo.appName()}`),
     platformVersion: IosInfo.platFormVersion(),
-    // app: IosInfo.appName() //use - path.resolve(`./apps/${IosInfo.appName()}`) if passing a custom app
-}];
+    resetOnSessionStartOnly: false,
+  },
+];
 
-config.cucumberOpts.tagExpression = '@iosApp';// pass tag to run tests specific to ios
+config.cucumberOpts.tagExpression = "@iosApp"; // pass tag to run tests specific to ios
 
 exports.config = config;
-
 
 // npm cache clean --force
